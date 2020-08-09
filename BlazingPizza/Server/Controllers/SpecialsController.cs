d4a7afedeb083637ef0fb8c70f71a006a30a14d8
LongDuongBIT <huulongduong@gmail.com>
2020-08-10 05:52:09 +0700

    master | Re-create the whole solution to support client running on backend startup

---
+++ BlazingPizza/Server/Controllers/SpecialsController.cs
@@ -0,0 +1,29 @@
+﻿using BlazingPizza.Server.Models;
+using BlazingPizza.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Server.Controllers
+{
+    [Route("specials")]
+    [ApiController]
+    public class SpecialsController : Controller
+    {
+        private readonly PizzaStoreContext _db;
+
+        public SpecialsController(PizzaStoreContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PizzaSpecial>>> GetSpecials()
+        {
+            List<PizzaSpecial> res = (await _db.Specials.ToListAsync()).OrderByDescending(x => x.BasePrice).ToList();
+            return res;
+        }
+    }
+}
---
+++ BlazingPizza/Server/Program.cs
@@ -0,0 +1,36 @@
+﻿using BlazingPizza.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Server
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            IHost host = CreateHostBuilder(args).Build();
+
+            // Initialize the database
+            IServiceScopeFactory scopeFactory = host.Services.GetRequiredService<IServiceScopeFactory>();
+            using (IServiceScope scope = scopeFactory.CreateScope())
+            {
+                PizzaStoreContext db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+                if (db.Database.EnsureCreated())
+                {
+                    SeedData.Initialize(db);
+                }
+            }
+
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
